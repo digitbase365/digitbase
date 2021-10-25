3c2869b5834af107e64ed60d9b5fa03b00f0489e
digitbase365 <darlsele@gmail.com>
2021-10-25 11:25:03 +0100

    Service Section Done

---
+++ db.js
@@ -0,0 +1,28 @@
+// Nav Menu Script
+    var marker = document.querySelector('.marker');
+    var item = document.querySelectorAll('nav a');
+
+
+   function indicator(e){
+     marker.style.left = e.offsetLeft+"px";
+     marker.style.width = e.offsetWidth+"px";
+   } 
+
+   item.forEach(link => {
+     link.addEventListener('mouseover', (e)=>{
+       indicator(e.target);
+     })
+   })
+// End of Nav Menu Script
+
+// Morphing Burger Icon{}
+   let menu = document.querySelector('.menu')
+   let burger = document.querySelector('.burger')
+
+   menu.addEventListener('click', ()=>{
+       menu.classList.toggle('swish')
+       burger.classList.toggle('swish')
+   })
+
+
+// End of Morphing Burger Icon{}
